d9f997dc16816485798959d6825c72b25d78a188
davidjoohnson <you@example.com>
2023-06-18 14:01:50 +0200

    pagina (base)

---
+++ src/components/Header.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import './header.css';
+
+const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    return (
+        <header className="header">
+            <button className="menu-toggle" onClick={toggleMenu}>
+                <i className={menuOpen ? 'fas fa-times' : 'fas fa-bars'}></i>
+            </button>
+            <h1 className="title">
+                <span>MyWeb</span> 
+                <span className="gradiente"> Currículum</span>
+            </h1>
+            <nav className={menuOpen ? 'nav open' : 'nav'}>
+                <ul className="nav-list">
+                    <li><a href="/">Página Principal</a></li>
+                    <li><a href="/sobremi">Sobre mí</a></li>
+                    <li><a href="/formacion">Formación</a></li>
+                    <li><a href="/experiencia">Experiencia profesional</a></li>
+                    <li><a href="/idiomas">Idiomas</a></li>
+                    <li><a href="/programas">Programas</a></li>
+                    <li><a href="/contacto">Contacto</a></li>
+                    <li><a href="/reloj">Reloj</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
