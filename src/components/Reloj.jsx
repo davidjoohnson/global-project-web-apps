d749305dd1e3f21d26cae7891ac14e5a48e707a1
davidjoohnson <you@example.com>
2023-06-18 14:20:43 +0200

    reloj con jsx

---
+++ src/components/Reloj.jsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react';
+import './reloj.css';
+
+const Reloj = () => {
+    const [hora, setHora] = useState('');
+    const [minutos, setMinutos] = useState('');
+    const [segundos, setSegundos] = useState('');
+
+    useEffect(() => {
+        const interval = setInterval(actualizarReloj, 1000);
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);
+
+    const actualizarReloj = () => {
+        const fecha = new Date();
+        const horaActual = formatoDosDigitos(fecha.getHours());
+        const minutosActual = formatoDosDigitos(fecha.getMinutes());
+        const segundosActual = formatoDosDigitos(fecha.getSeconds());
+
+        setHora(horaActual);
+        setMinutos(minutosActual);
+        setSegundos(segundosActual);
+    };
+
+    const formatoDosDigitos = (valor) => {
+        return valor < 10 ? '0' + valor : valor;
+    };
+
+    const cambiarColor = () => {
+        const letrasReloj = document.querySelectorAll('.reloj span');
+        letrasReloj.forEach((letra) => {
+            const color = generarColorAleatorio();
+            letra.style.color = color;
+        });
+    };
+
+    const generarColorAleatorio = () => {
+        const letras = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letras[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    };
+
+    return (
+        <div className="back-img">
+            <div className="container animate__animated animate__fadeIn">
+                <h1 className="title2">
+                    <a className="animate__animated animate__fadeIn">Reloj</a>
+                </h1>
+                <div className="cuadro2">
+                    <div className="reloj">
+                        <span id="hora">{hora}</span>
+                        <span className="puntos">:</span>
+                        <span id="minutos">{minutos}</span>
+                        <span className="puntos">:</span>
+                        <span id="segundos">{segundos}</span>
+                    </div>
+                    <button id="cambiarColor" onClick={cambiarColor}>
+                        Cambiar color
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Reloj;
